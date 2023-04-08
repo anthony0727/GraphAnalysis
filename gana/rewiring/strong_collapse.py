ef6530466bca38d94fe4604b402a22da36462d97
anthony0727 <jwseok0727@gmail.com>
2023-04-08 16:53:53 +0900

    add main & fix bugs

--- gana/rewiring/strong_collapse.py
+++ gana/rewiring/strong_collapse.py
@@ -3,15 +3,13 @@ import numpy as np
 import networkx as nx
 
 
-def strong_collapse(g):
+def strong_collapse(mat):
     """
     calculates persistent diagrams of a graph
 
     Strong Collapse for Persistence
     https://arxiv.org/pdf/1809.10945.pdf
     """
-    mat = nx.incidence_matrix(g).toarray()
-
     row_q = deque()
     col_q = deque()
     [row_q.append(row) for row in mat]
@@ -26,25 +24,45 @@ def strong_collapse(g):
                 continue
             nonzero_col = mat[:, nonzero_cols[0]]
             nonzero_rows = mat[np.where(nonzero_col != 0)[0]]
-            subset = np.where(np.all(nonzero_rows == v))[0]
-            mat = np.delete(mat, (subset), axis=0)
-            for i in nonzero_cols:
-                if not col_visited[i]:
-                    col_q.append(mat[:, i])
-                    col_visited[i] = True
+            v_index = -1
+            for i, arr in enumerate(nonzero_rows):
+                if np.all(arr == v):
+                    v_index = i
+                    break
+            if v_index != -1:
+            # subset = np.where(np.all(nonzero_rows == v))[0]
+                mat = np.delete(mat, (v_index), axis=0)
+                for i in nonzero_cols:
+                    if not col_visited[i]:
+                        col_q.append(mat[:, i])
+                        col_visited[i] = True
 
         while len(col_q) != 0:
-            v = col_q.pop()
+            tau = col_q.pop()
             nonzero_rows = np.where(v != 0)[0]
             if len(nonzero_rows) == 0:
                 continue
             nonzero_row = mat[:, nonzero_rows[0]]
             nonzero_cols = mat[np.where(nonzero_row != 0)[0]]
-            subset = np.where(np.all(nonzero_cols == v))[0]
-            mat = np.delete(mat, (subset), axis=0)
-            for i in nonzero_rows:
-                if not row_visited[i]:
-                    row_q.append(mat[:, i])
-                    row_visited[i] = True
+            tau_index = -1
+            for i, arr in enumerate(nonzero_cols):
+                if np.all(arr == tau):
+                    tau_index = i
+                    break
+            if tau_index != -1:
+            # subset = np.where(np.all(nonzero_cols == v))[0]
+                mat = np.delete(mat, (tau_index), axis=0)
+                for i in nonzero_rows:
+                    if not row_visited[i]:
+                        row_q.append(mat[i])
+                        row_visited[i] = True
 
     return mat
+
+
+if __name__ == '__main__':
+    g = nx.lollipop_graph(10, 5)
+    mat = nx.incidence_matrix(g).toarray()
+    print(mat.shape)
+    mat = strong_collapse(mat)
+    print(mat.shape)
